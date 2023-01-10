63c7b0ec7665d7d07833aa7cefaf5c500b92b458
Softlabltd.com <freelancernishad123@gmail.com>
2023-01-10 21:35:38 +0600

    আপডেট

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>AKKHOR | Home 1</title>
+    <title>আলোকিত তেতুলিয়া</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Favicon -->
